c8c2e456b16325fd16d62b98067eabdfc6439a00
DRK-Betreuungsdienste <michael.samaras@gmail.com>
2026-07-24 09:32:20 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'audioguide-v11';
+const CACHE = 'audioguide-v15';
 const CORE = ['./', './index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
